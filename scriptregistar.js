b8357a72886b6598eb776e68739a6c7410f374b2
Limtsu <95717013+Limtsu@users.noreply.github.com>
2022-04-01 00:31:16 +0100

    Add files via upload

---
+++ scriptregistar.js
@@ -0,0 +1,16 @@
+const multiStepForm = document.querySelector("[data-multi-step]")
+const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
+let currentStep = formSteps.findIndex(step => {
+  return step.classList.contains("active")
+})
+
+if (currentStep < 0) {
+  currentStep = 0
+  showCurrentStep()
+}
+
+function showCurrentStep() {
+  formSteps.forEach((step, index) => {
+    step.classList.toggle("active", index === currentStep)
+  })
+}
